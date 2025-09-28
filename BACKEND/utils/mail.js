5f8980806a0bb816e55a52109572331d9d0ee32c
amitkumar2407 <amitkumar9334978554@gmail.com>
2025-09-28 17:03:15 +0530

    food_delivery

--- BACKEND/utils/mail.js
+++ BACKEND/utils/mail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const config = require("../config/config");
 
 const transporter = nodemailer.createTransport({
-  service: "GMAIL",
+  service: "smtp.gmail.com",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
